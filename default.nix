9f27610f883b7f1b91fb060180516dbc44c22e4b
Felix C. Stegerman <flx@obfusk.net>
2018-10-19 21:26:42 +0200

    ...

--- default.nix
+++ default.nix
@@ -1,33 +1,39 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  buildNpmPackage = pkgs.callPackage ~/dev/nix-npm-buildpackage {};
+  bp = pkgs.callPackage ~/dev/nix-npm-buildpackage {};
+
   integreties = {
    "https://codeload.github.com/icholy/Duration.js/tar.gz/cb1c58efc2772ef0f261da9e2535890734a86417"         = "sha512-WZEMW8xDHHnxu2RK9y8YzSXgzOLveGPwRWEHSGYcEsLts52MN3M7lZaPLyZoHW8FOpVbM+2H5wnhOR+6RJZJIw==";
    "https://codeload.github.com/meirish/broccoli-sri-hash/tar.gz/5ebad6f345c38d45461676c7a298a0b61be4a39d"  = "sha512-G3Rs6Xbn8UXNJznKcEUn9kA6CTvyU2xdmZkEUJniSO9mLmBuMmBEhLJXIblYiOPjcO8l5YuiKDq/r49R6IvStA==";
    "https://codeload.github.com/meirish/ember-cli-sri/tar.gz/1c0ff776a61f09121d1ea69ce16e4653da5e1efa"      = "sha512-Rm87BsYdlZBYt/SdYq/ADmBpR7PsxIi1seuxaCsFuLIkr8c5ME7fdi9aRfwUFvbFMQStRu12BDfl9hs92EPY1A==";
   };
 
-# yarnOpts = "--offline --cache-folder ./npm-cache/_cacache";
+  node-sass-path = "node_modules/node-sass/vendor/linux-x64-64/binding.node";
+  node-sass = pkgs.fetchurl {
+    url     = "https://github.com/sass/node-sass/releases/download/v4.9.3/linux-x64-64_binding.node";
+    sha512  = "53b91bf2ea906b24834da9650b44db2fb3cd347ee6713a709f8f95fe996deaf4121c508c0e5d37be8601b667e763d4b804429735a43fdab8898f15dd383563ae";
+  };
 
-  vault-ui = src: buildNpmPackage {
-    inherit src;
-    useYarnLock     = true;
-    yarnIntegreties = integreties;
-    npmBuildMore    = ''
-     # npm ls
-     # exit 1
-     npm rebuild node-sass
-     npm run build-dev
+  vault-ui = src: bp.buildYarnPackage {
+    inherit src integreties;
+    yarnBuildMore = ''
+      # npm rebuild node-sass
+      mkdir -p `dirname ${node-sass-path}`
+      ln -s ${node-sass} ${node-sass-path}
+
+      mkdir _HOME
+      HOME=$PWD/_HOME yarn run build
+      rm -fr _HOME
+
+      mv ../pkg/web_ui _web_ui
     '';
-  # npmBuildMore    = "yarn ${yarnOpts} && npm rebuild node-sass && yarn ${yarnOpts} run build-dev";
-  # buildInputs     = [ pkgs.yarn ];
-    buildInputs     = [ pkgs.git ];
   };
 
   vault = pkgs.callPackage vault' {};
 
-  vault' = { stdenv, fetchFromGitHub, go, gox, removeReferencesTo }:
+  vault' = { stdenv, fetchFromGitHub, go, gox, go-bindata,
+             go-bindata-assetfs, removeReferencesTo }:
 
   let
     # Deprecated since vault 0.8.2: use `vault -autocomplete-install` instead
@@ -49,7 +55,11 @@ let
       sha256 = "0lckpfp1yw6rfq2cardsp2qjiajg706qjk98cycrlsa5nr2csafa";
     };
 
-    nativeBuildInputs = [ go gox removeReferencesTo ];
+    buildUI = true;
+
+    nativeBuildInputs = [ go gox removeReferencesTo ] ++
+                        stdenv.lib.optionals buildUI
+                        [ go-bindata go-bindata-assetfs ];
 
     preBuild = ''
       patchShebangs ./
@@ -60,11 +70,14 @@ let
       ln -s $(pwd) src/github.com/hashicorp/vault
 
       export GOPATH=$(pwd)
+    '' + (if buildUI then ''
+      rm -fr ui
+      ln -s ${vault-ui (src + "/ui")} ui;
+      mkdir -p pkg
+      ln -s ../ui/_web_ui pkg/web_ui
+    '' else "");
 
-      rm -fr ui/ && ln -s ${vault-ui (src+"/ui")} ui";
-    '';
-
-    buildPhase = "make static-assets dev-ui";
+    makeFlags = if buildUI then "static-assets dev-ui" else "";
 
     installPhase = ''
       mkdir -p $out/bin $out/share/bash-completion/completions
