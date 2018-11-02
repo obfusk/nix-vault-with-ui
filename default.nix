664d3d9a1f30626d0277a62c520cf82df0e0a2a1
Felix C. Stegerman <flx@obfusk.net>
2018-11-02 16:21:32 +0100

    pass pkgs as parameter

--- default.nix
+++ default.nix
@@ -1,10 +1,10 @@
 let
-  bp = pkgs.callPackage (fetchGit {
+  bp = pkgs: pkgs.callPackage (fetchGit {
     url = "https://github.com/serokell/nix-npm-buildpackage.git";
     rev = "1f607e575b6b313dc6ac7bc83313ef718d1e2184";
     ref = "1f607e5-tag-you-are-it"; # TODO: "v0.1.0"
   }) {};
-in { pkgs ? import <nixpkgs> {}, npm-buildpackage ? bp }:
+in { pkgs ? import <nixpkgs> {}, npm-buildpackage ? bp pkgs }:
 let
   integreties = {
    "https://codeload.github.com/icholy/Duration.js/tar.gz/cb1c58efc2772ef0f261da9e2535890734a86417"         = "sha512-WZEMW8xDHHnxu2RK9y8YzSXgzOLveGPwRWEHSGYcEsLts52MN3M7lZaPLyZoHW8FOpVbM+2H5wnhOR+6RJZJIw==";
